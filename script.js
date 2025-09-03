121de7295d1bb7ccc2da8c9714e832996d3cebbe
Canvaname30 <00001118706596sp@al.educacao.sp.gov.br>
2025-09-03 15:45:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     })
 
+    // Clark
+
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
